22078248807ea2a30cba6649fa4546976a371d83
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-10-03 02:43:18 +0530

    parsing csv file

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,57 @@
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import Papa, {ParseResult} from 'papaparse';
+ 
 
-function App() {
-
-
-  return (
-    <>
-      Hi
-    </>
-  )
+type Data = {
+  name: string
+  family: string
+  email: string
+  date: string
+  job: string
 }
 
-export default App
+function App() {
+  const [data, setData] = useState<Data[]>([]); // Use a generic type for the data state
+
+  useEffect(() => {
+    // Replace 'sales-data.csv' with the correct path to your CSV file
+    Papa.parse("/sales_data.csv", {
+      header: true,
+      download: true,
+      skipEmptyLines: true,
+      delimiter: ",",
+      complete: (results: ParseResult<Data>) => {
+        setData(results.data)
+      },
+    })
+    
+  }, []);
+
+  return (
+    <div>
+      <h1>CSV Data</h1>
+      <table>
+        <thead>
+          {data.length > 0 && (
+            <tr>
+              {Object.keys(data[0]).map((key) => (
+                <th key={key}>{key}</th>
+              ))}
+            </tr>
+          )}
+        </thead>
+        <tbody>
+          {data.map((row, index) => (
+            <tr key={index}>
+              {Object.values(row).map((value, colIndex) => (
+                <td key={colIndex}>{value}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
